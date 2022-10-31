f3a99e13780a298c93c003a7d42db8efd187d6b2
Prithvi <bhatprithvi@rediffmail.com>
2022-10-31 23:42:49 +0100

    Submission_v1

--- project/pid_controller/pid_controller.cpp
+++ project/pid_controller/pid_controller.cpp
@@ -27,7 +27,8 @@ void PID::Init(double Kpi, double Kii, double Kdi, double output_lim_maxi, doubl
   PError = 0;
   IError = 0;
   DError = 0;
-  new_delta_time = 0;
+  //this-> new_delta_time = 0;
+  preverr = 0;
 }
 
 
@@ -35,13 +36,35 @@ void PID::UpdateError(double cte) {
    /**
    * TODO: Update PID errors based on cte.
    **/
-  
-  if (new_delta_time>0)
-  {
-    DError = (cte-PError)/new_delta_time ;
-  }
-  IError += cte* new_delta_time;
+  IError += cte* this ->new_delta_time;
   PError = cte;
+  
+  /*The next few lines of code include the integral windup for tuning PID gains. This is done by preventing the integral term from accumulating above or below pre-determined bounds as mentioned in https://en.wikipedia.org/wiki/Integral_windup#cite_note-4.
+  The codes are based on http://brettbeauregard.com/blog/2011/04/improving-the-beginner%e2%80%99s-pid-reset-windup/
+  and https://github.com/BanaanKiamanesh/Anti-Windup-PID-Controller/blob/main/PID.cpp
+  */
+  if (output_lim_maxi > PError)
+      lim_max_integ = output_lim_maxi - PError;  // Limit maximum integral bounds
+  else
+      lim_max_integ = 0.0;
+
+  if (output_lim_mini < PError)
+     lim_max_integ = output_lim_mini - PError; // Limit minimium integral bounds
+  else
+        lim_min_integ = 0.0;
+
+    // Limit Integrator gain
+     if (IError > lim_max_integ)
+        IError = lim_max_integ;
+
+     else if (IError < lim_min_integ)
+         IError = lim_min_integ;
+  
+  if (this ->new_delta_time>0)
+  {
+    DError = (cte-preverr)/this->new_delta_time ;
+  }
+  preverr = cte;
 }
 
 double PID::TotalError() {
